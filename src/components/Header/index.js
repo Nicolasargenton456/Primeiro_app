4fd70822f046daf4890af8b670443cad8de8c256
Nicolasargenton456 <ds.46276553808@gmail.com>
2024-02-23 15:15:45 -0300

    componentizando o scroll view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,32 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import Logo from "../../assets/logo.png"
-import { Feather, AntDesign, FontAwesome5 } from '@expo/vector-icons';
+import { Feather, AntDesign, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Header() {
     return (
         <View
-            style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                width: "100%",
-                alignItems: "center"
-            }}
-        >
-            <Image style={{ height: 60, width: 200 }} source={Logo} />
-            <View style={{ flexDirection: "row" }} />
+        style={styles.container}>
+        <Image style={{width:200, height:60}} source={Logo}/>
+        <View style={styles.icon}>
             <TouchableOpacity>
-                <Feather name="message-circle" size={24} color="black" />
-            </TouchableOpacity><FontAwesome5 name="heart" size={24} color="black" />
-
+                <Feather name="heart" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <MaterialCommunityIcons style={styles.icon} name="facebook-messenger" size={24} color="black"/>
+            </TouchableOpacity>
+            </View>    
         </View>
-    )
+    );
 }
 
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        alignItems: "center"  
+    },
+    icons:{flexDirection:"rom"},
+    icons:{marginLeft:8}
 
-
-
+})
